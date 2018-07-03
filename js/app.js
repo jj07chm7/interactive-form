59d30c9a78d125b340eb56b366337c852a820e38
Jon Yu <jy300x2@gmail.com>
2018-07-02 22:09:02 -0400

    form validation

--- js/app.js
+++ js/app.js
@@ -135,21 +135,49 @@ payment.addEventListener("change", (event) => {
   }
 });
 
-// If any of the following validation errors exist, prevent the user from submitting the form:
-// Name field can't be blank.
-// Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example.
-// User must select at least one checkbox under the "Register for Activities" section of the form.
-// If the selected payment option is "Credit Card," make sure the user has supplied a Credit Card number, a Zip Code, and a 3 number CVV value before the form can be submitted.
-// Credit Card field should only accept a number between 13 and 16 digits.
-// The Zip Code field should accept a 5-digit number.
-// The CVV should only accept a number that is exactly 3 digits long.
-// NOTE: Make sure your validation is only validating Credit Card info if Credit Card is the selected payment method.
-
 const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+const totalCheckboxes = checkboxes.length;
+const ccNumber = document.getElementById("cc-num");
+const zipCode = document.getElementById("zip");
+const cvv = document.getElementById("cvv");
+
 form.addEventListener("submit", (event) => {
   if (nameInput.value === '') {
-    //e.preventDefault();
-    nameInput.style.border = "thick solid #0000FF";
+    nameInput.style.border = "0.5px solid #FF0000";
     event.preventDefault();
+  }
+  if (email.value.indexOf('@') == -1) {
+    email.style.border = "0.5px solid #FF0000";
+    event.preventDefault();
+  }
+  let checked = 0;
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      checked += 1;
+    }
+  }
+  if (checked === 0) {
+    //alert("You must select one activity");
+    const errorMessage = document.createElement("h4");
+    errorMessage.style.color = 'red';
+    errorMessage.innerHTML = "You must select one activity.";
+    showTotal.appendChild(errorMessage);
+    event.preventDefault();
+  }
+  if (payment.value === 'credit card') {
+    if (!ccNumber.value || ccNumber.value.length <= 13 || ccNumber.value.length >= 16) {
+      ccNumber.style.border = "0.5px solid #FF0000";
+      event.preventDefault();
+    }
+    if (!zipCode.value || zipCode.value.length !== 5) {
+      zipCode.style.border = "0.5px solid #FF0000";
+      event.preventDefault();
+    }
+    if (!cvv.value || cvv.value.length !== 3) {
+      cvv.style.border = "0.5px solid #FF0000";
+      event.preventDefault();
+    }
   }
 })
